971e83d21851df32ace6b69ec43432e0c4db9b4e
davide chiffi <davide.chiffi@maprota.com>
2024-08-14 00:06:42 +0200

    Widgets Whtsapp und Instagram

---
+++ socialmedia.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('whatsapp-icon').addEventListener('click', function() {
+        window.open('https://wa.me/4917631129075', '_blank');
+    });
+    document.getElementById('instagram-icon').addEventListener('click', function() {
+        window.open('https://www.instagram.com/moeckmuehler_feuerwerke/', '_blank');
+    });
+});
